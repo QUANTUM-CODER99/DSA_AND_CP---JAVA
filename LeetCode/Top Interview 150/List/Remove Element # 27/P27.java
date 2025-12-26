70da37086b5b428051eba4993191d29377c5b219
QUANTUM-CODER99 <aishwaryvishwakarma30oct2006@gmail.com>
2025-12-27 00:24:36 +0530

    P27.java

---
+++ LeetCode/Top Interview 150/List/Remove Element # 27/P27.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        List<Integer> ans = new ArrayList<>();
+        
+        for(int i=0 ; i<nums.length ; i++)
+        if(nums[i]!=val)
+        ans.add(nums[i]);
+
+        for(int i=0 ; i<ans.size() ; i++)
+        nums[i] = ans.get(i);
+        
+        return ans.size();
+    }
+}
