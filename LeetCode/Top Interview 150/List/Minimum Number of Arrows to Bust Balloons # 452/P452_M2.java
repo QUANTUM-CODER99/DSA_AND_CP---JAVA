79fadfc50676cb5ba022d79f24c8a93217258bb9
QUANTUM-CODER99 <aishwaryvishwakarma30oct2006@gmail.com>
2025-12-26 23:45:40 +0530

    P452_M2.java

---
+++ LeetCode/Top Interview 150/List/Minimum Number of Arrows to Bust Balloons # 452/P452_M2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public static int findMinArrowShots(int[][] points) {
+        if (points == null || points.length == 0)
+            return 0;
+
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+
+        int arrows = 1;
+        long end = points[0][1];
+
+        for (int i = 1; i < points.length; i++) {
+            // If current balloon starts after the last arrow end
+            if (points[i][0] > end) {
+                arrows++;
+                end = points[i][1];
+            }
+        }
+
+        return arrows;
+    }
+}
