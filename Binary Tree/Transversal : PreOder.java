f43133b5dd11b47e11ad04c0c851d67d0a35ae51
QUANTUM-CODER99 <aishwaryvishwakarma30oct2006@gmail.com>
2025-12-29 02:59:37 +0530

    Transversal : PreOder

---
+++ Binary Tree/Transversal : PreOder.java
@@ -0,0 +1,10 @@
+    // 1 - PRE-ORDER
+    public static void preorder(Node root) {
+        if (root == null) {
+            System.out.print(-1 + " ");
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
