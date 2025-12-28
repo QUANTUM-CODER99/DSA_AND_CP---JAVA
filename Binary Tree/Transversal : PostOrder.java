220c10eb61d22de28e1d81ce725122a990ee7ad3
QUANTUM-CODER99 <aishwaryvishwakarma30oct2006@gmail.com>
2025-12-29 03:01:55 +0530

    Transversal : PostOrder

---
+++ Binary Tree/Transversal : PostOrder.java
@@ -0,0 +1,10 @@
+    // 3 - POST-ORDER
+    public static void postorder(Node root) {
+        if (root == null) {
+            System.out.print(-1 + " ");
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
