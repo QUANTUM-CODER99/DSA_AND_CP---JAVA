0baf24a3d7e768b21e13c5468214f8adeb2a1464
QUANTUM-CODER99 <aishwaryvishwakarma30oct2006@gmail.com>
2025-12-29 03:09:55 +0530

    BinaryTree : Height

---
+++ Binary Tree/BinaryTree : Height.java
@@ -0,0 +1,10 @@
+    // FINDING THE HEIGHT OF THE TREE
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
