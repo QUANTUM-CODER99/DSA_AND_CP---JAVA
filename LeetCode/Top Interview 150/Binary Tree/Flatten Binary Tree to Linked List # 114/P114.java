f11c85170bf1d3b3f116721d7906bcd8cf6db870
QUANTUM-CODER99 <aishwaryvishwakarma30oct2006@gmail.com>
2026-01-03 00:03:42 +0530

    P114.java

---
+++ LeetCode/Top Interview 150/Binary Tree/Flatten Binary Tree to Linked List # 114/P114.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null)
+            return;
+
+        List<TreeNode> nodes = new ArrayList<>();
+        preorder(root, nodes);
+
+        for (int i = 0; i < nodes.size() - 1; i++) {
+            nodes.get(i).left = null;
+            nodes.get(i).right = nodes.get(i + 1);
+        }
+    }
+
+    private void preorder(TreeNode root, List<TreeNode> nodes) {
+        if (root == null)
+            return;
+
+        nodes.add(root);
+        preorder(root.left, nodes);
+        preorder(root.right, nodes);
+    }
+}
