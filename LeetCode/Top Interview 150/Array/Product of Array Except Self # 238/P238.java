241b1aa68479684f600538ffd52ef13f4c8dc25a
QUANTUM-CODER99 <aishwaryvishwakarma30oct2006@gmail.com>
2026-01-08 00:41:57 +0530

    P238.java

--- LeetCode/Top Interview 150/Array/Product of Array Except Self # 238/P238.java
+++ LeetCode/Top Interview 150/Array/Product of Array Except Self # 238/P238.java
@@ -1,22 +1,19 @@
 class Solution {
-    public static int[] productExceptSelf(int[] nums) {
+    public int[] productExceptSelf(int[] nums) {
         int output[] = new int[nums.length];
-        int left = 1, right = 1;
+        // CORE CONCEPT -> output[i] = (prefix product index i) * (suffix product index i);
 
-        for (int i = nums.length-1; i > -1; i--) {
-            int r = nums[i];
-            output[i] = right; 
-            right *= r;
+        // FOR FINDING THE PREFIX SUM ->
+        output[0] = 1;
+        for(int i=1;i<nums.length;i++)
+            output[i] = output[i-1] * nums[i-1];
+
+        // FOR FINDING THE SUFFIX SUM ->
+        int rightProduct = 1;
+        for(int i=nums.length-1 ; i>-1; i--) {
+            output[i] *= rightProduct;
+            rightProduct *= nums[i];
         }
-
-        for(int i=0;i<nums.length;i++) {
-            int l = nums[i];
-            nums[i] = left;
-            left *= l;
-        }
-
-        for (int i = 0; i < nums.length; i++)
-            output[i] = nums[i] * output[i];
 
         return output;
     }
